0328b504542561692623c440ad8f70a71e921eb6
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-09-10 20:24:48 -0400

    one more question re: spec

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -8,6 +8,7 @@ describe '#position_taken? in lib/position_taken.rb' do
     expect{position_taken?(board, position)}.to_not raise_error
   end
 
+ #is this the desired behavior? if someone tries to fill out a position that doesn't exist, we should return false? not raise some kind of error or message?
   it 'returns false if the submitted position is not a valid location on the board, i.e. not included in the range of 1-9.' do
     board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     position = 100
