0c2f8d0e6be052e549512b0f36bfa0a08c97b6e3
Tekhaus Dev <dev@tekha.us>
2016-09-06 07:32:09 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    taken = true
+  else
+    taken = false
+  end
+  taken
+end
