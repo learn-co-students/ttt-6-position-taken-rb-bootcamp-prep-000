08e2aa8c2f328de44e1f4c60405b249194ebc45a
Justin Mayerick <jmayeric@gmail.com>
2016-10-12 13:18:27 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -7,19 +7,19 @@
 #
 # end
 
-# def position_taken?(board, index)
-#   if board[index] == "X" || "O"
-#     true
-#   elsif board[index] == " " || "" || nil
-#     false
-#   end
-# end
-
 def position_taken?(board, index)
-  if board[index] == "X" || "O"
+  if board[index] == "X" || board[index] == "O"
     true
-  end
-  if board[index] == " " || "" || nil
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
     false
   end
 end
+
+# def position_taken?(board, index)
+#   if board[index] == "X" || board[index] == "O"
+#     true
+#   end
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     false
+#   end
+# end
