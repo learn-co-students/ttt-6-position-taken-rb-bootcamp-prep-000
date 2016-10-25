b55133b639b15c63abc123ac1cadb50715fa314c
Kathy Shin <katchat143@gmail.com>
2016-10-25 06:52:37 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
-# code your #position_taken? method here!
+def position_taken?(array, index)
+ taken = nil
+  if (array[index] ==  " " || array[index] ==  "" || array[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+  taken
+end
