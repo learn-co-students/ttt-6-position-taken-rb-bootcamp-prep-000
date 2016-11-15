ebbdb717e901a58a7a14b912acfc85a417cd6c25
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-15 03:49:51 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(b, idx)
+    if b.length <= idx
+        return false
+    elsif b[idx] == "" || b[idx] == nil
+        return false
+    elsif b[idx] == " "
+        return false
+    else
+        return true
+    end
+end
