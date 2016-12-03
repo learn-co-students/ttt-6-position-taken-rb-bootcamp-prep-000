44d5a570429d0ad52beb63a7363bf7ddf0472ab2
Pak Belalang <opblang@yandex.com>
2016-12-03 18:11:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+  reval = true
+
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    retval = false
+  elsif (board[index] == "X") || (board[index] == "O")
+    retval = true
+  end
+  retval
+end
