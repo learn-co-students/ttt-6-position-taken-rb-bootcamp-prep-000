a65e82745b680b83c1e6360e3090ace1be66035f
Daniel Lempesis <danlrcn@hotmail.com>
2016-12-05 19:00:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-# code your #position_taken? method here! 
+# code your #position_taken? method here!
 def position_taken?(board,index)
   if board[index] == " "  ||  board[index] == ""  ||  board[index] == nil
     return false
