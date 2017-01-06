82a19a763aa3ece03df7d8522daefead9887514b
Scott Plunkett <scottyplunkett@gmail.com>
2017-01-06 20:48:12 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+    if board[index] == "X" || board[index] == "O"
+        return true
+    else board[index] == "   " || board[index] ==  ""
+        return false
+    end
+end
