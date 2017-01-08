8935d3fd1710e6bf9570cac0a518d511c4f909ce
Grant MacManus <flatiron@macdav.net>
2017-01-08 18:38:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
