05d6e5f5a7ef3da218a2424614f611bf41366484
Megan McLean <mmclean@protonmail.com>
2017-01-15 06:20:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
-# code your #position_taken? method here!
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 15: Tic Tac Toe Position Taken
+
+# Define a method, 'position_taken?', that accepts two parameter(s):
+# 'board', an array, and 'index', the index the user wishes to fill with their
+# token ("X" or "O").
+# If the board index is "", " " or nil, return false (position not taken).
+# Else, return true (position taken).
+
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  end
+  return true
+end
