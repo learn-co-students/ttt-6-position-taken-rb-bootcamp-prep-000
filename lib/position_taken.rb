493f266f1a681aa460fdf5fe957711eefc3b40a6
Michael Bennett <michael.c.bennett@live.com>
2017-01-16 00:28:41 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == nil || (board[index] == " " || board[index] == "")
+    false
+  else
+    true
+  end
+end
