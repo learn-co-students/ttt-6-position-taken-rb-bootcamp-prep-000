b55c24b36e1bbc6d02df8f9b2a94b2b9d2a29739
Michael Bennett <michael.c.bennett@live.com>
2017-01-16 00:29:50 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,4 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == nil || (board[index] == " " || board[index] == "")
-    false
-  else
-    true
-  end
+  board[index] == nil || (board[index] == " " || board[index] == "") ? false : true
 end
