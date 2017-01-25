accd54bec008ef80fe44d0a6341856695a1ee448
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-25 21:29:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+if board[index]  == !(" " || "" )
+   false
+elsif (board[index] == "X") || (board[index] == "0")
+  true
+else
+  false
+end
+end
