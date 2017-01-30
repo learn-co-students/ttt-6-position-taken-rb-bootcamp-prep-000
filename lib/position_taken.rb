7926a836a0d4cbb317be04222c36e6ac3670a8e8
Nadine Fischoff <nadine.khattak@gmail.com>
2017-01-30 20:56:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, player_index)
+  if board[player_index]== " " || board[player_index] == "" ||  board[player_index] == nil
+    false
+  else
+    true
+ end
+end
