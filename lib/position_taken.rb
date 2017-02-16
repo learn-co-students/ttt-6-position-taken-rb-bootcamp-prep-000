0629e1fbf6acee9556fc5f619b7b81e13cd05ecd
Vagelis Kalimeris <aka9080@yahoo.gr>
2017-02-16 14:05:54 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(board, n)
+  if board[n] == "" || board[n] == " " || board[n] == nil
+    false
+  elsif board[n] == "X" || board[n] == "O"
+    true
+  end
+end
