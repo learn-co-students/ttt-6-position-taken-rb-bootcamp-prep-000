d38be12ed3c83d31413fb1d189d5f4d883c576a4
Daniel Park <dpeace926@gmail.com>
2017-03-07 18:00:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  board.each do |index|
+    if index == " " || index == "" || index == nil
+      return false
+    elsif index == "X" || index == "O"
+      return true
+    end
+
+  end
+end
