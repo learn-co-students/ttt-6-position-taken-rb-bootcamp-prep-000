9a18c0664dbdf3074eadf6ae876fa25fd2879efc
isaac LEVI <isaaclevi1710@gmail.com>
2017-04-25 00:48:19 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(arrayBoard , index.to_i - 1)
+def position_taken?(arrayBoard , index)
   if (arrayBoard[index] == " " || arrayBoard[index] == "" || arrayBoard[index] == nil)
     return false
   else
