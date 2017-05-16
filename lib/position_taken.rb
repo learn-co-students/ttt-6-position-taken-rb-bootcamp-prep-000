593e599537c46ee2a0d97c1ac414522f083be46e
Renard Burnett <renxburnett@gmail.com>
2017-05-16 17:10:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+
+def position_taken?(board_array, current_index)
+  if board_array[current_index] == "X" || "O"
+    result = true
+  end
+  if board_array[current_index] == " " || "" || nil
+    result = false
+  end
+  return result
+end
