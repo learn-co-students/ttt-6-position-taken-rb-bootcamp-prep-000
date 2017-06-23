70e49e6e55fb31fa7cbcbca0b8949c0e056ff273
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-23 21:17:51 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  case board[index]
+  when ""
+    return false
+  when " "
+    return false
+  when nil
+    return false
+  end
+  if "X" || "O"
+    return true
+  end
+end
