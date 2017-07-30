14c9306875c8d781ba15a214e314b50c2c28668d
Anastasia  Barkett <anastasia.barkett@gmail.com>
2017-07-30 21:24:00 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  taken = nil
+  if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
+  end
+end
