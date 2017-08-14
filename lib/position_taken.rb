6dea19c3ebffad31558121c21c9833f484adf23f
hiroki urushidate <kirohitedashiruu@gmail.com>
2017-08-14 01:51:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, num)
+=begin
   if board[num] == " " || board[num] == nil ||  board[num]==""
     false
   else
     true
   end
+=end
+(board[num] == " " || board[num] == "" || board[num] ==nil) ? false:true
 end
