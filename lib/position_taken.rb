090f26e74a9d3d5720ea120b1a712b3b4b55929d
Anthony Giuliano <agiuliano@gmail.com>
2017-08-20 22:48:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, space)
+  moves = %W(X O)
+  if moves.include?(board[space])
+    return true
+  else
+    return false
+  end
+end
