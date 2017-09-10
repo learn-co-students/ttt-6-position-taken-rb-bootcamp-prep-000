20c44166addc623a123495483cad83ad2efb79d9
John Day <itisjohnday@gmail.com>
2017-09-10 17:55:51 +0000

    Done.

--- spec/position_taken_spec.rb
+++ spec/position_taken_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/position_taken.rb'
+require_relative './spec_helper.rb'
 
 describe '#position_taken? in lib/position_taken.rb' do
   it 'accepts a board and the index to check as arguments' do
