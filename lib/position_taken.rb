03b840ab9384f3eb8ad90ef427ee7777aae12bbd
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 17:21:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, token)
+  if board[token] == " " || board[token] == "" || board[token] == nil
+    false
+  elsif board[token] =="X" || board[token] == "O"
+    true
+  end
+end
