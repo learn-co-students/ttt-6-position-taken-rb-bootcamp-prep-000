6263f5d4611d3322e47a32e834141a5426ca0b05
Brandon Hosley <brandon.hosley@csuglobal.edu>
2017-10-08 23:13:24 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+#  if board[index] == " " || array[index] == "" || array[index] == nil
+#    return false
+#  else
+#    return true
+#  end
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
+  end
+end
