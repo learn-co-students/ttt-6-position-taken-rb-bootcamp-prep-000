2e630892a8c8e231ae5959a7b29107cfa65426bd
Damian Guillory <guillorywdamian@gmail.com>
2017-10-22 21:41:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, index)
+    if array[index] == " "
+        false
+    elsif array[index] == ""
+        false
+    elsif array[index] == nil
+        false
+    else
+        true
+    end
+end
