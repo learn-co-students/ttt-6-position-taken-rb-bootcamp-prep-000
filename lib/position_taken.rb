6cbb0e0ffcb4f1f17ba73b4852939882b3ecc282
Annette Michaels <micha232@umn.edu>
2017-11-02 08:53:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  spot = board[index]
+  if (spot == "X" || spot == "O")
+    return true
+  else return false
+  end
+end
