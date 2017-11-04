cabbc9d871f31a814919d521fa74ff8d598aa3f2
Danny Vega <dannycvega@gmail.com>
2017-11-04 17:53:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+
+index_number = index - 1
+
+if board[index]== "" || board[index]== " " || board[index]== nil
+  return false
+end
+
+
+if board[index]== "O" || board[index]== "o" || board[index]== "X" || board[index]== "x"
+  return true
+end
+
+
+end# code your #position_taken? method here!
+
+borple = ["X"]
+position_taken?(borple,0)
