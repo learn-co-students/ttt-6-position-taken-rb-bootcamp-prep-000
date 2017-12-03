9b3e98002698f7a84c9a51ef6d50fc8d8cbcc210
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-12-03 06:43:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  converted = index.to_i
+  taken = nil
+
+  if board[converted] == "X" || board[converted] == "O"
+    taken = true
+
+  elsif board[converted] == "" || " "
+    taken = false
+  end
+  return taken
+end
