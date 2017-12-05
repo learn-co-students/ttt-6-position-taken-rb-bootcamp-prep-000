7d241f0a05fca149562b8d8c2ffdc9682a23d98b
Hyeok Jung Kim <hyeokjungkim@gmail.com>
2017-12-05 03:17:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index.to_i] == " " || board[index.to_i] == "" || board[index.to_i] == nil
+    return false
+  else
+    return true
+  end
+end
