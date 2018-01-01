a50a8f2c560367f25e0e78ccbaed242cd39e1186
michael lynn <michael_lynn@comcast.net>
2018-01-01 06:24:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,29 @@
 # code your #position_taken? method here!
+ttt_board = ["",""," "," ","X"," "," "," ",""]
+
+def position_taken?(board, index)
+  # basic solution:
+
+  # taken = nil
+  # if (board[index] ==  " " || board[index] == "" || board[index] == nil)
+  #   taken = false
+  # else
+  #   taken = true
+  # end
+  # taken
+
+  # advanced solution w/ ternary operator
+  
+  # This was flatiron School's solution (Learn.co) whereas mine was simpler
+  #! (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+  #  false : true
+
+  #This was mine:
+  ! (board[index] ==  " " || board[index] == "" || board[index] == nil) 
+end
+
+puts position_taken?(ttt_board, 0)
+puts position_taken?(ttt_board, 1)
+puts position_taken?(ttt_board, 2)
+puts position_taken?(ttt_board, 3)
+puts position_taken?(ttt_board, 4)
