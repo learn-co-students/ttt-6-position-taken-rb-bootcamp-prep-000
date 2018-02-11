7d7345aa0422a6e69c12a39ca54afb08ef0c8b1f
Yung Leung <yungl.leung1@gmail.com>
2018-02-11 19:31:02 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken? (boardArray, index)
+  if boardArray[index] == " " || boardArray[index] == "" || boardArray[index] == nil
+    return false
+  else
+    return true
+  end
+end
