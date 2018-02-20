6aa109de7a6b4390aec268d36ce47bbfddf15753
Oleksandr Diuriagin <sashadg@gmail.com>
2018-02-20 03:54:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+def position_taken?(arr,pos)
+  if (arr[pos] == " " || arr[pos] == "" || arr[pos] == nil)
+    return false
+  else return true
+  end
+end
