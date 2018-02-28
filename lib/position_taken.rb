21558a4e222463e5ee5941017b85e5cd83e09d94
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-28 17:40:37 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,22 @@
 # code your #position_taken? method here!
+
+
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def position_taken? (board, index)
+  
+    # must check each variation of nil independently. i.e.  if board[index] == (" " || "" || nil) doesn't work because only the first check (in this case " " will be checked)
+
+    if board[index] == (" ") || board[index] == ("") || board[index] == (nil)
+      
+      return false
+    
+    else 
+      
+      return true
+      
+    end  
+
+end
