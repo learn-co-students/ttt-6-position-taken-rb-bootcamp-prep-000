b8ca5ecb80029ece17cbc6ec14f06b10842708ab
Everett <everett.bowline@gmail.com>
2018-03-09 02:42:18 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
