bee1fe32d4b7c091c62f583fc6d5c2d41ab5aa61
Chris Dundon <chris.dundon@gmail.com>
2018-03-18 02:13:22 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,23 @@
 # code your #position_taken? method here!
  
-def position_taken(board, idx)
-  occupant = board[idx].strip
-  if occupant == ""
+def position_taken?(board, idx)
+  
+  if board.size == 0
+    return
+  end
+  
+  out = board[idx]
+  nil_ = (nil == board[idx])
+  
+  if nil_
     return false
+  end
+  
+  out_strip = out.strip
+  space = ("" == out_strip)
+  if space
+    return false
+  elsif out_strip == "X" || out_strip == "O"
+    return true
   end
 end
