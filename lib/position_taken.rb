5e1f55aa6e2a4759cfee935d32fbbf1c75699814
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-26 03:08:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  end
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
+end
