b98e4c451f5e703a272ef5f844203e284f1364e1
Valerie Moore <periwinkle17@outlook.com>
2018-05-22 21:19:26 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+def position_taken?(board,index)
+  if board[index] == " "
+    false
+    elsif board[index] == ""
+    false
+    elsif board[index] == nil
+    false
+    elsif board[index] == "X"
+    true
+    elsif board[index] == "O"
+    true
+  else
+      nil
+end
+end
