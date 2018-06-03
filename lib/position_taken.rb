97f89e2a576a47ad41c1595d20a03d213b72b65e
Sushi Chanrai <sushi@pathworks.co>
2018-06-03 03:20:08 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, input_to_index)
+  smorg = board[input_to_index]
+  if (smorg == " " || smorg == "" || smorg == nil)
+   return false
+  elsif smorg == "X" || "O"
+    return true
+  end
+end  
