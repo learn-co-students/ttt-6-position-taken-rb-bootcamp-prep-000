095397f055c5b45f819c0d96dd054d8b56a84ce3
Danny O'B <dannyk85.dob@gmail.com>
2018-06-19 11:56:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board_array, index)
+  if board_array[index] == " " or board_array[index] == "" or board_array[index] == NIL
+    return false
+  else
+    return true
+  end
+end
