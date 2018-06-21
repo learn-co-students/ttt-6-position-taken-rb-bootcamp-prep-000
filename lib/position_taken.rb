92e62f8feab34a0ae410e2a471feec20bcdc3ddc
Loren Cerri <plexidev@gmail.com>
2018-06-21 18:18:15 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if !(!!board[index])
+    false
+  elsif board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  else
+    true
+  end
+end
