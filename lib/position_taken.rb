b0b637c44a0a96a50d31f00cfbfaa58fb2cbe6a2
nip.adrian@gmail.com Nip <nip.adrian@gmail.com>
2018-06-25 01:11:59 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  end
+  if board[index] == ""
+    return false
+  end
+  if board[index] == nil 
+    return false
+  end
+  if board[index] == "X" || board[index] == "O"
+    return true 
+  end
+end
