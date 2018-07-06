3458a123db4775950b2ece79eb076172c79a0f0c
sehsehco <joanna@sehseh.co>
2018-07-06 16:06:56 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,3 @@
 def position_taken?(board, index)
-  if board[index] === ' ' || board[index] === '' || board[index] === nil
-    false
-  elsif board[index] === 'X' || board[index] === 'O'
-    true
-  end
+  (board[index] === ' ' || board[index] === '' || board[index] === nil) ? false : true
 end
