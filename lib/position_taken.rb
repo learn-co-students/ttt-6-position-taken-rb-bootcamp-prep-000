b0cb20a680f10590222c0e476ffa8f1f1cdba844
Glen Simpson <gsimpson@airmaster.com.au>
2018-07-06 11:40:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  if(index == nil || board[index] == nil)
+    return FALSE
+  end
+  if(board[index].upcase == "X" || board[index].upcase == "O")
+    return TRUE
+  else
+    return FALSE
+  end
+end
