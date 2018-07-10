566bb41e7d70f5fe0dae8f63ae51d90df5e25de0
Jeffrey Rowe <jeffrowetull@gmail.com>
2018-07-10 02:44:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken?(array, index)
+  if array[index] == " " || array[index] == ""|| array[index] == nil
+    return false
+  end
+  if array[index] == "X" || array[index] == "O"
+    return true
+  end
+end
