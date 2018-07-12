0a318a002a14a0f20ff6d74271678badd5095e8d
Jing Cao <jingcao.yale@gmail.com>
2018-07-12 16:06:05 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
-def position_taken?(board,index)
-  if (board[index] == "X" || board[index] == "O")
+def valid_position?(board,index)
+  if !(board[index] == "X" || board[index] == "O")
     return true
   else
     return false
