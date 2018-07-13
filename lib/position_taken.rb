c01c159031c9d0b452342c3f32bcfafcd061bc07
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-07-13 13:56:15 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+
+# def position_taken?(board, index)
+#   taken = nil
+#   if board[index] == " " || board[index] == "" || board[index] == nil
+#     false
+#   else
+#     true
+#   end
+# end
+
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
