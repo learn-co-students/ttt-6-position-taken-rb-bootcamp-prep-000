156991ac0e0cda8a8611280722b127ac39e8c0d5
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-22 13:08:13 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+
+require 'pry'
+
+def position_taken?(board, index)
+  # if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+  if (!board[index]) || (board[index].strip == "")
+    false
+  else
+    true
+  end
+end
+
+binding.pry
