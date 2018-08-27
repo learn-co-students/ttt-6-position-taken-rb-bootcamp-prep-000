6b5065da3d706564c9dd94b891999c4641b092fc
Travis Tokuyama <travistokuyama@gmail.com>
2018-08-27 02:13:24 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, char)
+  if (board[char] == " " || board[char] == "" || board[char] == nil)
+    false
+  elsif (board[char] == "X" || board[char] == "O")
+    true
+  end
+end
