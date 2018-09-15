667939ca023d99923d2a1fe9da086c622cd7ce3d
Alex S <alex.sunga@fliptech.co>
2018-09-15 09:04:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+  isTaken = nil
+  
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+    isTaken = false 
+  elsif
+    isTaken = true 
+  end
+  
+  isTaken
+  
+end
