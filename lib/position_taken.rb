0eb65c0baff9cfd15a1f6f703ed850e1837a1da1
Edwin Lin <edwinlin@gmail.com>
2018-11-08 22:42:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, desired_position)
+  if [" ", "", nil].include?board[desired_position]
+    return false
+  end
+  if ["X", "O"].include?board[desired_position]
+    return true
+  end
+end
