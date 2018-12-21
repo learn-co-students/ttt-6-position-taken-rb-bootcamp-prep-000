14518062246196fe74ad3f1536b1962cecc406c6
Burak Kocak <burakkocak884@ghotmail.com>
2018-12-20 22:00:51 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,14 @@
+def position_taken?(board, index)
+ if board[index] == " "
+return false
+elsif board[index] == ""
+	return false
+elsif board[index] == nil
+	return false
+else
+	return true
+end
+	 end
+
+
 # code your #position_taken? method here!
