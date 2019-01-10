de94b48b06b7a8f0662be499b1cedf7ec3a9b00a
Joan in DC <joanrigdon@gmail.com>
2019-01-10 16:24:05 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,18 @@
-# code your #position_taken? method here!
+require "pry"
+
+def position_taken?(board, square)
+  if board[square] == " " || board[square] == "" || board[square] == nil
+    return false
+  else
+    return true
+  end
+end
+
+#Failures:
+#1) #position_taken? in lib/position_taken.rb returns true if
+#   the board has a value of "X" or "O" in the index
+# Failure/Error: expect(position_taken?(board, index)).to 
+# be(true)
+
+       #expected true
+        #    got false
