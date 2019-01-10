da828a49934ad1f29210896a1d28b6d18606d268
Joan in DC <joanrigdon@gmail.com>
2019-01-10 16:28:28 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,11 +8,3 @@ def position_taken?(board, square)
   end
 end
 
-#Failures:
-#1) #position_taken? in lib/position_taken.rb returns true if
-#   the board has a value of "X" or "O" in the index
-# Failure/Error: expect(position_taken?(board, index)).to 
-# be(true)
-
-       #expected true
-        #    got false
