99ae287ff1b447a7a5af4e3530518183e416ee6f
Joe Muro <jmuro0321@gmail.com>
2019-01-18 19:41:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  position = board[index]
+  
+  if !(position.nil? || position.eql?(" ") || position.empty?)
+    return (position.eql?("X") || position.eql?("O"))
+  else
+    return false
+  end
+  
+end
