1a728041c24862d3a87a1975282acf9118c979a6
github username <crishanks12@gmail.com>
2019-01-28 17:17:56 -0800

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    board.each do |space|
+        if space == 'X' || space == 'O'
+            return true;
+        end
+    end
+    false
+end
