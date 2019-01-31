eb9a11d54b583f9651073b6e1c8e5c02c0d5b368
Andreas P. Gortsilas <apgortsilas@aggate.gr>
2019-01-31 12:43:29 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+
+def position_taken?(arr, index)
+  # !(arr[index] == '' || arr[index] == ' ' || arr[index] == nil)
+  # arr[index] != '' && arr[index] != ' ' && arr[index] != nil
+  !['', ' ', nil].include?(arr[index])
+end
