175c6163d8954cfa68f70721f1529951462783b9
David zhang <d.ydzdavid@yahoo.com>
2019-02-27 05:55:33 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here
+
+
+def position_taken?(board, location)
+   index = board[location.to_i - 1]
+    if  index == " " || index == "" || index == nil
+    return false  
+  end
+  else if  index == "X" || index == "O"
+    return true  
+  end
+end
