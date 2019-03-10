fbff154f4dcd6bd9934a7919aface4d29654aba8
Meg Galvez <meg@oakmaine.com>
2019-03-10 20:04:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,11 @@
 def position_taken?(board, index)
 
+<<<<<<< HEAD
  ! (board[index] == " " || board[index] == "" || board[index] == nil)
+
+    
+   
+>>>>>>> 08506d7f57eb9391d0aadf97837aac319a4e587b
 
 end
 
