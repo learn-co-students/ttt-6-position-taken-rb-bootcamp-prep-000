3f6ec9852bd4065676f7cdc06627aa6ee21c40ef
Carly Pecora <carly@carlypecora.com>
2019-04-11 12:47:20 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
-# code your #position_taken? method here!
+def position_taken?(array, char)
+	if array[char] == "" || array[char] == " " || array[char].nil?
+		return false
+	else array[char] == "X" || array[char] == "O"
+		return true
+	end
+end
