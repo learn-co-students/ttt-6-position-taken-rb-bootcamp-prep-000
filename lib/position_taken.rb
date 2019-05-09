d5c0f761bc592b7978242a3d1817857c219d0590
Jonathan Koss <jonathan_koss@yahoo.com>
2019-05-09 15:55:48 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,9 +4,6 @@ def position_taken?(board, index)
 if board[index] == " " || board[index] == "" || board[index] == nil
   return false
 else
-  if board[index] == "X" || board[index] == "O"
     return true
   end
-  end
-  
 end 
