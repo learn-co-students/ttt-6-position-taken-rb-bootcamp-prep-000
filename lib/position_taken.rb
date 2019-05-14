3698f0722885b7b0de538dae0e8c1e6d85175223
William Chang <wdchang86@gmail.com>
2019-05-14 23:04:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(array, index)
   if array[index] == " " || array[index] == "" || array[index] == nil
-    FALSE
+    false
   else
-    TRUE
+    true
   end
 end
