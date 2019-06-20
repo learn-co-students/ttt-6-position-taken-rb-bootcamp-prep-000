8a9efc4a77c0d1e8a7ba882d780fc6d7debeef71
Danny Lee <dreamer@panix.com>
2019-06-20 20:38:35 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken? (board, index)
+  if (board[index] == "X" || (board[index]) == "O") 
+    return TRUE
+  end
+  if (board[index] == " " || board[index] == "" || board[index] == NIL)
+    return FALSE
+  end
+
+end
