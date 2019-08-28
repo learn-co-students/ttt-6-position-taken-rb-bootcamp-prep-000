85372b66d729faf961b0e9e2e50cd2d34b2ec3aa
Sarah Elzinga <sarah.elzinga@flatironschool.com>
2019-08-28 11:36:01 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index)
+
+    # taken = nil
+    # if board[index] == "" || board[index ] == " " || board[index] == nil
+    #     taken = false
+    # else
+    #     taken = true
+    # end
+
+    # taken
+
+    (board[index] == "" || board[index] == " " || board[index] == nil) ? 
+    false : true
+    # taken
+
+end
