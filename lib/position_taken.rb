fe92b5c7c63cff342f42f80b23557a3bf79258bb
William Damm <wdamm@powerhrg.com>
2020-03-21 01:05:25 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,19 @@
-# code your #position_taken? method here!
+def position_taken?(board, index)
+  
+  
+  if board[index] == "X"
+  return true
+
+  elsif board[index] == "O"
+  return true
+
+  elsif board[index] == " "
+  return false
+  
+else 
+  return false
+  
+end
+  
+  
+end
