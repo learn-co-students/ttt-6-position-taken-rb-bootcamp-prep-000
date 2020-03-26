515126915a5694b27fd8b71602e9ff11e3b00151
James Jensen <jnjensen@live.com>
2020-03-26 02:09:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+
+def position_taken?(array, token_index)
+  if array[token_index] == " " || array[token_index] == "" || array[token_index] == nil
+    return false
+  
+  elsif array[token_index] == "X" || array[token_index] == "O"
+    return true
+    
+  end
+end
