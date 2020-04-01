3e4e43dca432730e7834ce0727e4848c879c5756
dapper-hardware-8759 <huoliangchen2@gmail.com>
2020-04-01 19:05:52 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(borad, index)
+  if(borad[index] == " " || borad[index] == "" || borad[index] == nil)
+    return false
+  else
+    return true
+  end
+end
