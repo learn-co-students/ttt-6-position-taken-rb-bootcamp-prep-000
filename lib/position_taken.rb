aedbedd70fc153d0f775a64281f71f3cb069d2fc
Stefan Sokolowski <stefansokolowski16@gmail.com>
2020-09-09 11:07:10 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -9,12 +9,4 @@ def position_taken?(board, index)
   end
 end
 
-#def position_taken?(board, index)
-#  if board[index] == " " || board[index] == "" || board[index] == nil
-#    false
-#  elsif board[index] == "X" || board[index] == "O"
-#    true
-#  end
-#end
-
 puts position_taken?(board, 9)
