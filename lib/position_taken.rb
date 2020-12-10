c5b4fbe6db823aecb9b8c7a9128d451498f23f12
Nahomie ND <nducasse3@gmail.com>
2020-12-10 15:35:23 +0000

    Define a method that updates an array passed into it as an argument. Define a method that uses control flow to manipulate an array. Method is responsible for evaluating the user's input against the Tic Tac Toe board and check to see whether or not that position is occupied.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,17 @@
 # code your #position_taken? method here!
+require "pry"
+
+def position_taken?(board, index)
+  binding.pry
+if (board[index] == "X" || board[index] == "O")
+    true
+    
+      elsif board[index]
+     false
+     
+  else board[index] == nil
+    false
+  end
+end
+
+ #(int % 3 == 0 && int % 5 == 0)
